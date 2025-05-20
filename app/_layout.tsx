2b7828645e37823f54141c01f84b5f01f7d40c73
Davido264 <67966122+Davido264@users.noreply.github.com>
2025-05-20 10:20:41 -0500

    hello :3

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import "@/global.css"
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
